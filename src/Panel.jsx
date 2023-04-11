217085ba0e45e3d5d350e713a32f81a6b55634e3
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2023-04-11 13:39:58 -0500

    Add increment button.

--- src/Panel.jsx
+++ src/Panel.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
+let count = 0
 export default function Panel() {
+  const [state, setState] = useState()
+  count++
   return (
     <>
-      <h2>More Whirled</h2>
+      <h2>More Whirled, num renders {count}</h2>
+      <button onClick={setState}>Set State</button>
     </>
   )
 }
